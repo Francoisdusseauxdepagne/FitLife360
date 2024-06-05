21f4bce7eb479ddf257b4fa9b61b0e9ba2ace1f7
Francois Dusseaux-Depagne <francoisdusseaux-depagne@MacBook-Air-de-Francois.local>
2024-06-05 11:27:38 +0200

    05/06

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -45,7 +45,7 @@ class RegistrationController extends AbstractController
             // generate a signed url and email it to the user
             $this->emailVerifier->sendEmailConfirmation('app_verify_email', $user,
                 (new TemplatedEmail())
-                    ->from(new Address('mailer@echappeesExotiques.com', 'echappees Exotiques'))
+                    ->from(new Address('mailer@fitlife360.com', 'FitLife360'))
                     ->to($user->getEmail())
                     ->subject('Please Confirm your Email')
                     ->htmlTemplate('registration/confirmation_email.html.twig')
