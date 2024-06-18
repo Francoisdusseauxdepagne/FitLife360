1500f22f33df875978895d150c131ff640162dca
Francois Dusseaux-Depagne <francoisdusseaux-depagne@MacBook-Air-de-Francois.local>
2024-06-18 16:31:04 +0200

    18/06

--- src/Form/ContactEventType.php
+++ src/Form/ContactEventType.php
@@ -6,14 +6,20 @@ use App\Entity\ContactEvent;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class ContactEventType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('content', null, ['label' => 'Message'])
-        ;
+            ->add('content', TextType::class, [
+                'label' => 'Message',
+                'data' => 'Je souhaite réserver pour cet événement', // Valeur prédéfinie
+                'attr' => [
+                    'readonly' => true, // Rend le champ en lecture seule (optionnel)
+                ],
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
