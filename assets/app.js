0eb91ce446a2c381a01604458e06562f8e62f4c1
Francois Dusseaux-Depagne <francoisdusseaux-depagne@MacBook-Air-de-Francois.local>
2024-06-19 11:37:56 +0200

    19/06

--- assets/app.js
+++ assets/app.js
@@ -1,6 +1,11 @@
 // Styles globaux (si nécessaire)
 import './styles/app.scss';
+import './styles/nav.scss';
+
+// Styles particuliers
+import './styles/home.scss';
 import './styles/profile.scss';
+
 
 // Démarrage de Stimulus (si utilisé)
 import './bootstrap';
