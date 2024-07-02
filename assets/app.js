a16dda931faa63d8492f753cfaa756ef420cb10c
Francois Dusseaux-Depagne <francoisdusseaux-depagne@MacBook-Air-de-Francois.local>
2024-07-02 09:52:02 +0200

    02/07

--- assets/app.js
+++ assets/app.js
@@ -1,5 +1,6 @@
+import './bootstrap.js';
 // Styles globaux (si nécessaire)
-import './styles/bootstrap.scss';
+import './styles/app.scss';
 import './styles/global.scss';
 
 // Styles particuliers
@@ -9,8 +10,7 @@ import './styles/sign.scss';
 import './styles/btn.scss';
 import './styles/js.scss';
 
-// Démarrage de Stimulus (si utilisé)
-import './bootstrap';
 
-require('@fortawesome/fontawesome-free/css/all.min.css');
+
 require('bootstrap');
+require('@fortawesome/fontawesome-free/css/all.min.css');
